1990715d0f9410825def0dbc3f3b96676de24da8
olardaniil <olardaniil@yandex.ru>
2024-07-27 15:42:51 +0300

    v2.3.8

--- backend-api/internal/service/s_inbound_clients.go
+++ backend-api/internal/service/s_inbound_clients.go
@@ -26,7 +26,7 @@ func (s *InboundClientService) AddInboundClient(inboundId int, newClient *model.
 		return "", false, err
 	}
 
-	key := fmt.Sprintf("vless://" + newClient.ID + "@185.225.201.103:443?type=tcp&security=reality&pbk=hR_tQr8FVdSOM-k7pt4oSGtjct6FfPvKNQMDzIDvjB8&fp=chrome&sni=microsoft.com&sid=a5ee0427&spx=%2F&flow=xtls-rprx-vision#Alpha_VPN-" + newClient.Email)
+	key := fmt.Sprint("vless://" + newClient.ID + "@185.225.201.103:443?type=tcp&security=reality&pbk=hR_tQr8FVdSOM-k7pt4oSGtjct6FfPvKNQMDzIDvjB8&fp=chrome&sni=microsoft.com&sid=a5ee0427&spx=%2F&flow=xtls-rprx-vision#Alpha_VPN-" + newClient.Email)
 
 	return key, res, nil
 }
